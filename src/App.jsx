c0078ea16c03d5c3f25ac66a66ad95a9f33eeabb
Grigory <102112036+GrigoryGerasimov@users.noreply.github.com>
2022-09-01 22:52:38 +0200

    Revert "Revert "Devrouting""

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Users } from "./components/Users.jsx";
+import { Route, Switch } from "react-router-dom";
+import Login from "./layouts/Login.jsx";
+import Main from "./layouts/Main.jsx";
+import UsersList from "./components/UsersList.jsx";
+import NavBar from "./components/NavBar.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 export const App = () => {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/login" component={Login} />
+                <Route path="/users/:userId?" component={UsersList} />
+                <Route component={NotFound} />
+            </Switch>
+        </>
+    );
 };
--- src/layouts/Users.jsx
+++ src/layouts/Users.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { UsersTable } from "./UsersTable.jsx";
-import { Pagination } from "./Pagination.jsx";
+import { UsersTable } from "../components/UsersTable.jsx";
+import { Pagination } from "../components/Pagination.jsx";
 import { paginate } from "../utils/paginate.js";
-import { GroupList } from "./GroupList.jsx";
+import { GroupList } from "../components/GroupList.jsx";
 import PropTypes from "prop-types";
-import api from "../api/index.js";
-import { SearchStatus } from "./SearchStatus";
+import api from "../api";
+import { SearchStatus } from "../components/SearchStatus.jsx";
+import { SearchBar } from "../components/SearchBar.jsx";
 import getPagesRange from "../utils/getPagesRange.js";
 import _ from "lodash";
 
-export const Users = () => {
+const Users = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -35,12 +36,24 @@ export const Users = () => {
     const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
     const handlePageChange = (pageIndex) => setCurrentPage(pageIndex);
 
+    const [searchValue, setSearchValue] = useState("");
+    const handleResetSearch = () => {
+        setSearchValue("");
+    };
     const handleProfessionSelect = (item) => {
         setSelectedProf(item);
+        handleResetSearch();
     };
     const clearFilter = () => {
         setSelectedProf();
+        handleResetSearch();
     };
+    const handleSearch = ({ target }) => {
+        const { value } = target;
+        clearFilter();
+        setSearchValue(value);
+    };
+
     const handleSort = (item) => {
         setSortBy(item);
     };
@@ -51,9 +64,10 @@ export const Users = () => {
         setCurrentPage(1);
     }, [selectedProf]);
 
-    const filteredUsers = selectedProf ? users.filter((user) => JSON.stringify(user.profession) === JSON.stringify(selectedProf)) : users;
-    const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
-    const count = filteredUsers.length;
+    const updatedUsers = users.filter((user) => selectedProf ? JSON.stringify(user.profession) === JSON.stringify(selectedProf) : searchValue ? user.name.toLowerCase().includes(searchValue.toLowerCase()) : user);
+
+    const sortedUsers = _.orderBy(updatedUsers, [sortBy.path], [sortBy.order]);
+    const count = updatedUsers.length;
     const userCrop = paginate(sortedUsers, currentPage, pageSize);
     useEffect(() => {
         const [, pages] = getPagesRange(count, pageSize);
@@ -82,6 +96,7 @@ export const Users = () => {
                 )}
                 <div className="d-flex flex-column">
                     <SearchStatus length={count}/>
+                    <SearchBar value={searchValue} onSearch={handleSearch} />
                     {count ? <UsersTable users={userCrop} onSort={handleSort} selectedSort={sortBy} onDelete={handleDelete} onBookmarkToggle={handleToggleBookmark}/> : null}
                     <div className="d-flex justify-content-center">
                         <Pagination
@@ -96,6 +111,8 @@ export const Users = () => {
         );
     } else return "loading...";
 };
+
+export default Users;
 
 Users.propTypes = {
     users: PropTypes.arrayOf(PropTypes.object)
